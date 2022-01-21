7d52285b1d337de4fa42d385a44549f20c85f28f
g-prog <alaobukky2@gmail.com>
2022-01-21 04:03:34 -0800

    concluded with the services section

--- tedbree/src/Services.jsx
+++ tedbree/src/Services.jsx
@@ -1,6 +1,7 @@
 
 
 import "./services.css"
+import AppsIcon from '@mui/icons-material/Apps';
 
 function Services() {
     return (
@@ -144,17 +145,95 @@ function Services() {
                                 <div className='help'>
                                     <div className='inner'>
                                         <div className="one"><p>1</p></div>
-                                        <div className="info"><h2>How can Aria Help Your Business</h2></div>
+                                        <div className="info">
+                                            <h2>How can Aria Help Your Business</h2>
+                                            <div className="paragraph">
+                                                <p>At Aria Solutions, we've taken the consultancy concept one step further
+                                                    by offering a full service management organization with expertise.
+                                                </p>
+
+                                            </div>
+                                            
+                                        </div>
+
+                                        
                                     
                                     </div>
+
+                                        <div className="two">
+                                            <div className="number">
+                                                <p>2</p>
+                                            </div>
+
+                                            <div className="letters">
+                                                <h3>Get Strategic Business Planning</h3>
+                                            </div>
+                                        </div>
+
+                                        <div className="two">
+                                            <div className="number">
+                                                <p>3</p>
+                                            </div>
+
+                                            <div className="letters">
+                                                <h3>Online Marketing Campaigns</h3>
+                                            </div>
+                                        </div>
                                     
-                                </div>
+                                    </div>
+
+                                    
+                                    
+                                    
+                                 
                                
                             </div>
                         </div>
                         <div className='down'>
                             <div className='down-left'>
-                                hello
+                                <div className="upper">
+                                <div className="business">
+                                    <div className="icon"><AppsIcon style={{fontSize: "20px", color: "#0ecea4" }}/></div>
+                                    <div className="pitch"><h4 className='tag'>Business</h4></div>
+                                </div>
+
+                                <div className="business">
+                                    <div className="icon"><AppsIcon style={{fontSize: "20px", color: "gray" }}/></div>
+                                    <div className="pitch"><h4>Expertise</h4></div>
+                                </div>
+
+                                <div className="business">
+                                    <div className="icon"><AppsIcon style={{fontSize: "20px", color: "gray" }}/></div>
+                                    <div className="pitch"><h4>Quality</h4></div>
+                                </div>
+                                </div>
+
+                                <div className='companies'>
+                                    <h3>Business Services for Companies</h3>
+                                    <div className="pitch2">
+                                        <p>Aria provides the most innovative and customized business services
+                                            in the industry. Our <a href="#services">Services</a> section shows
+                                            how flexible we are for all types of budgets.
+                                        </p>
+                                    </div>
+
+                                    <div className="development">
+                                        <div className="speech"><h4>Business Development 100%</h4></div>
+                                        <div className="percentage"></div>
+                                    </div>
+
+                                    <div className="development">
+                                        <div className="speech2"><h4>Opportunity Spotting 76%</h4></div>
+                                        <div className="percentage2"></div>
+                                    </div>
+
+                                    <div className="development3">
+                                        <div className="speech3"><h4>Online Marketing 90%</h4></div>
+                                        <div className="percentage3"></div>
+                                    </div>
+                                </div>
+
+                               
                             </div>
                             <div className='down-right'>
                                 <img src='../assets/boy.jpg' alt='placeholder-img'/>
