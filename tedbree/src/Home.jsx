7995cba650c7636e55dabee1d86320a6dd45c3c6
g-prog <alaobukky2@gmail.com>
2022-01-25 07:45:04 -0800

    started with making the website mobile responsive

--- tedbree/src/Home.jsx
+++ tedbree/src/Home.jsx
@@ -1,10 +1,30 @@
-
-
+import React from "react";
+import { useState, useEffect } from "react"
 import "./home.css"
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Home() {
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+  
+        window.addEventListener('resize', changeWidth)
+  
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+  
+    }, [])
+
     return (
         <div className="home" id="home">
             <div className="hero">
@@ -17,37 +37,44 @@ function Home() {
                     <div className="logo">
                         <h4>Aria</h4>
                     </div>
+                    <div className="hamburger">
+                        <MenuIcon  onClick={toggleNav} style={{fontSize: "20px" }}/>
 
-                    <div className="links">
-                        <ul>
-                            <li><a href="#home">HOME</a></li>
-                            <li> <a href="#intro">INTRO</a></li>
-                            <li> <a href="#services">SERVICES</a></li>
-                            <li><a href="#contact">CALL ME</a></li>
-                            <li><a href="#projects">PROJECTS</a></li>
-                            <li><a href="#about">ABOUT</a></li>
-                            <li><a href="#contact2">CONTACT</a></li>
-                        </ul>
-
-                        <div className="social">
-                            <div className="hexagon">
-                                <div className="icon">
-                                   <FacebookIcon style={{fontSize: "20px" }}/>
-
-                                </div>
-                               
-
-                            </div>
-
-                            <div className="hexagon">
-                                <div className="icon">
-                                <TwitterIcon style={{fontSize: "20px" }}/>
-
-                                </div>
-                            </div>
-                            
-                        </div>
                     </div>
+                    {(toggleMenu || screenWidth > 600) && (
+                         <div className="links">
+                         <ul>
+                             <li><a href="#home">HOME</a></li>
+                             <li> <a href="#intro">INTRO</a></li>
+                             <li> <a href="#services">SERVICES</a></li>
+                             <li><a href="#contact">CALL ME</a></li>
+                             <li><a href="#projects">PROJECTS</a></li>
+                             <li><a href="#about">ABOUT</a></li>
+                             <li><a href="#contact2">CONTACT</a></li>
+                         </ul>
+ 
+                         <div className="social">
+                             <div className="hexagon">
+                                 <div className="icon">
+                                    <FacebookIcon style={{fontSize: "20px" }}/>
+ 
+                                 </div>
+                                
+ 
+                             </div>
+ 
+                             <div className="hexagon">
+                                 <div className="icon">
+                                 <TwitterIcon style={{fontSize: "20px" }}/>
+ 
+                                 </div>
+                             </div>
+                             
+                         </div>
+                     </div>
+
+                     )}
+                   
                     
                 </nav>
                 <div className="body">
